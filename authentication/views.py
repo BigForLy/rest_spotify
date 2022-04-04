6c3524b0c4c79644c7e09ca3be8217cdab20d084
BigForLy <oringan@mail.ru>
2022-04-04 12:13:14 +0500

    17

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from rest_framework.views import APIView
+
+from authentication.utils import HashCheck
+from rest_spotify import settings
+
+
+class AuthTelegram(APIView):
+
+    def get(self, request):
+        bot_token = settings.BOT_TOKEN
+        secret = bot_token.encode('utf-8')
+        if not HashCheck(request.GET, secret).check_hash():
+            return render(request, 'error.html', {
+                'msg': 'Bad hash!'
+            })
+        # user = TgUser.make_from_dict(request.GET)
+        # user.save()
+        # request.session['user_id'] = user.id
+        return redirect('/')
