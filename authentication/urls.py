4ffab0e52d51e50cf7f306086d7ab272bf26b839
BigForLy <oringan@mail.ru>
2022-04-05 16:09:13 +0500

    23

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,5 +4,4 @@ from authentication import views
 
 urlpatterns = [
     path('success/', views.LoginTelegram.as_view()),
-    # path('success/', views.AuthTelegram.as_view()),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,37 +1,9 @@
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
-
-# Create your views here.
-from rest_framework.views import APIView
-
 from authentication.models import User
 from authentication.utils import HashCheck
 from rest_spotify import settings
-
-
-class AuthTelegram(APIView):
-
-    def get(self, request):
-        bot_token = settings.BOT_TOKEN
-        secret = bot_token.encode('utf-8')
-        if not HashCheck(request.GET, secret).check_hash():
-            return render(request, 'error.html', {
-                'msg': 'Bad hash!'
-            })
-        # user, created = TelegramUser.objects.get_or_create(
-        #     tg_id=request.GET.get('id'),
-        #     )
-        user, created = User.objects.get_or_create(
-            telegram_chat_id=request.GET.get('id'),
-        )
-        user.username = request.GET.get('username'),
-        user.photo_url = request.GET.get('photo_url'),
-        user.auth_date = request.GET.get('auth_date')
-        user.save()
-        request.session['user_id'] = user.id
-        request.user = user
-        return redirect('song')
 
 
 class LoginTelegram(LoginView):
@@ -46,10 +18,12 @@ class LoginTelegram(LoginView):
         user, created = User.objects.get_or_create(
             telegram_chat_id=request.GET.get('id'),
         )
-        user.username = request.GET.get('username')
+        if user.username:
+            user.username = request.GET.get('username')
+        else:
+            user.username = f'User_{user.telegram_chat_id}'
         user.photo_url = request.GET.get('photo_url')
         user.auth_date = request.GET.get('auth_date')
         user.save()
-        # request.session['user_id'] = user.id
         login(request, user)
         return redirect('song')
