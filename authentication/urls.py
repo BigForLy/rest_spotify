43da2785b9b01d94bef2308e42049876086e2d1e
BigForLy <oringan@mail.ru>
2022-04-09 23:28:06 +0500

    35

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from authentication import views
 
 urlpatterns = [
-    path('success/', views.LoginTelegram.as_view()),
+    path('', views.LoginTelegram.as_view()),
 ]
