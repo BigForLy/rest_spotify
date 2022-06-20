376df0752e408bb132c85c94acbae4325007e3da
BigForLy <oringan@mail.ru>
2022-06-20 16:31:56 +0500

    feat: created model tests

---
+++ tests/test_model.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from telegram_api_integration import TelegramClient
+from dotenv import load_dotenv
+import os
+
+
+class TestSendTelegramMessageText(TestCase):
+
+    def test_incorrect(self):
+        telegram_chat_id = 1
+        message = 1
+        bot_token = 1
+        result = TelegramClient(telegram_chat_id,  bot_token) \
+            .send_message(message)
+        self.assertEqual(result.status_code, 404)
+
+    def test_correctly(self):
+        load_dotenv()
+        telegram_chat_id = os.getenv('ADMIN_TELEGRAM_CHAT_ID')
+        message = 'test message'
+        bot_token = os.getenv('BOT_TOKEN')
+        result = TelegramClient(telegram_chat_id,  bot_token) \
+            .send_message(message)
+        self.assertEqual(result.status_code, 200)
