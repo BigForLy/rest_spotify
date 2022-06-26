167c99d4e74dbacf262044c9864b67eb96d43d32
BigForLy <oringan@mail.ru>
2022-06-26 20:57:10 +0500

    patch: refactoring

--- song/views.py
+++ song/views.py
@@ -14,12 +14,12 @@ class HomePageView(generic.TemplateView):
     template_name = "all_song.html"
 
     def get_context_data(self, **kwargs):
-        
-        import redis
-        r = redis.Redis(host='redis-18045.c124.us-central1-1.gce.cloud.redislabs.com',
-        port=18045,
-        password='WJWHltvrSY4OtwkyogwIx9iDLjildkXv')
-        r.set('r', 'm')
+
+        # import redis
+        # r = redis.Redis(host='redis-18045.c124.us-central1-1.gce.cloud.redislabs.com',
+        #                 port=18045,
+        #                 password='WJWHltvrSY4OtwkyogwIx9iDLjildkXv')
+        # r.set('r', 'm')
         context = super().get_context_data(**kwargs)
         last_write = DataFromSpotify.objects.last()
         if last_write is None or last_write.is_overdue:
@@ -45,17 +45,18 @@ class SongSendMessageView(View):
     def post(self, request):
 
         # downloader = DownloadStrategy.music(request.POST.get("type"), request.POST.get("release"))
-        
+
         if request.META.get('HTTP_X_REQUESTED_WITH') != 'XMLHttpRequest':
             return JsonResponse({'status': 'Not AJAX request'}, status=404)
 
         if request.user.is_authenticated:
-            downloader = DownloadStrategy.music(request.POST.get("type_1"), request.POST.get("release"))
+            downloader = DownloadStrategy.music(
+                request.POST.get("type_1"), '7FIWs0pqAYbP91WWM0vlTQ')
             song: Song = downloader.download()
             with song as audio:
                 result: Response = TelegramClient(568817064, settings.BOT_TOKEN) \
                     .send_audio(audio, f'{song.artist} - {song.name}')
-            
+
             return JsonResponse({'status': result.reason}, status=result.status_code)
         else:
             return JsonResponse({'status': 'Please login'}, status=401)
