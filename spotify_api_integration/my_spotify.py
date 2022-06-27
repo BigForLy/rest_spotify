cae0e67a0b318d9590f08dbbed4a78eab4761f6c
BigForLy <oringan@mail.ru>
2022-06-27 23:57:15 +0500

    feat: create album song list

--- spotify_api_integration/my_spotify.py
+++ spotify_api_integration/my_spotify.py
@@ -29,15 +29,22 @@ class MySpotify(AbstractSpotify):
             'Content-Type': 'application/json',
             'Authorization': 'Bearer %s' % self.token
         }
-        self.__url = 'https://api.spotify.com/v1/browse/new-releases?limit=50'
 
-    def get_new_releases(self):
-        result = self.__requests(self.__url)
+    def get_new_releases(self) -> dict:
+        url = 'https://api.spotify.com/v1/browse/new-releases?limit=50'
+        response_json = self.__requests(url)
+        result = response_json.get('albums').get('items')
+        if response_json.get('albums').get('next'):
+            result += self.__requests(response_json.get('albums').get('next'))
         return result
 
-    def get_info_about(self, url):
-        response = requests.get(url=url, headers=self.__headers)
-        response_json = response.json()
+    def get_subreleases(self, parent_pk) -> dict:
+        url = f'https://api.spotify.com/v1/albums/{parent_pk}'
+        response_json = self.__requests(url)
+        return response_json.get('tracks').get('items')
+
+    def get_info_about_song(self, url) -> Song:
+        response_json = self.__requests(url)
         return Song(
             response_json['name'],
             response_json['artists'][0]['name'],
@@ -45,10 +52,6 @@ class MySpotify(AbstractSpotify):
             ""
         )
 
-    def __requests(self, url):
+    def __requests(self, url) -> dict:
         response = requests.get(url=url, headers=self.__headers)
-        response_json = response.json()
-        result = response_json.get('albums').get('items')
-        if response_json.get('albums').get('next'):
-            result += self.__requests(response_json.get('albums').get('next'))
-        return result
+        return response.json()
